6626d5d69e79d3d137e397834ae7c0e4ff7d9398
Arya Wiratama <aryawiratama2401@gmail.com>
2025-11-21 15:11:23 +0700

    update: generate qr (not done)

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
-from flask import Flask, render_template
-from sources.generate import generate_qr
-
-data = generate_qr("https://indonesia.com", "indonesia")
+from flask import Flask, render_template, request
+from sources import generate_qr
 
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('index.html')
+    data_res = None
+    if request.method == 'POST':
+        get_link = request.form.get('link')
+        get_nama = request.form.get('nama')
+
+        generate_qr(get_link, get_nama)
+        data_res = get_nama
+
+    return render_template('index.html', data_res=data_res)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=1245)
--- sources/generate.py
+++ sources/generate.py
@@ -4,10 +4,10 @@ import os
 
 def generate_qr(link, name_qr):
 
-    if not os.path.exists('assets'):
-        os.mkdir('assets')
+    if not os.path.exists('./static/images'):
+        os.mkdir('./static/images')
 
-    file_path = f'assets/{name_qr}.png'
+    file_path = f'./static/images/{name_qr}.png'
 
     qr = qrcode.QRCode()
     qr.add_data(link)
